c8a4e0b04aafe8a9a3290c1f80cb5ccbe8827f31
Diego Maroto <diego.diemg@gmail.com>
2025-05-20 09:46:35 +0200

    feat: Adding custom domain and root link with cf

--- terraform/portfolio.tf
+++ terraform/portfolio.tf
@@ -69,6 +69,8 @@ resource "aws_cloudfront_cache_policy" "portfolio_1d" {
 
 # CloudFront distribution
 resource "aws_cloudfront_distribution" "portfolio" {
+  depends_on          = [aws_acm_certificate_validation.portfolio]
+  aliases             = ["portfolio.marotodiego.com"]
   enabled             = true
   is_ipv6_enabled     = true
   comment             = "marotodiego.com Portfolio static site"
@@ -95,6 +97,47 @@ resource "aws_cloudfront_distribution" "portfolio" {
     }
   }
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn      = aws_acm_certificate_validation.portfolio.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+}
+
+data "aws_route53_zone" "portfolio" {
+  name         = "marotodiego.com"
+  private_zone = false
+}
+
+resource "aws_acm_certificate" "portfolio" {
+  provider          = aws.cloudfront
+  domain_name       = "marotodiego.com"
+  validation_method = "DNS"
+}
+
+resource "aws_route53_record" "portfolio_cert_validation" {
+  for_each = { for dvo in aws_acm_certificate.portfolio.domain_validation_options : dvo.domain_name => dvo }
+
+  zone_id = data.aws_route53_zone.portfolio.zone_id
+  name    = each.value.resource_record_name
+  type    = each.value.resource_record_type
+  ttl     = 300
+  records = [each.value.resource_record_value]
+}
+
+resource "aws_acm_certificate_validation" "portfolio" {
+  provider                = aws.cloudfront
+  certificate_arn         = aws_acm_certificate.portfolio.arn
+  validation_record_fqdns = [for record in aws_route53_record.portfolio_cert_validation : record.fqdn]
+}
+
+resource "aws_route53_record" "portfolio" {
+  zone_id = data.aws_route53_zone.portfolio.zone_id
+  name    = ""
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.portfolio.domain_name
+    zone_id                = aws_cloudfront_distribution.portfolio.hosted_zone_id
+    evaluate_target_health = false
   }
 }
